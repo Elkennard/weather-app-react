1b998dea587447c33cf85eace3be9e2779d0e1f2
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-06-19 14:08:21 +0100

    react and axios installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import axios from 'axio';
 
 function App() {
+
+  const url =`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=4e989d2bab3ac720d2bede89c770f680`
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
     </div>
   );
 }
